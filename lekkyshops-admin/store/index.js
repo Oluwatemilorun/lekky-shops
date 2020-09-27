ac73392345a584e7533e3fc33e6277ccfd3bf9fc
Theo Iyonor <symplytheo@gmail.com>
2020-09-27 12:49:45 +0100

    Moved nav links to store

---
+++ lekkyshops-admin/store/index.js
@@ -0,0 +1,29 @@
+export const state = () => ({
+  links: [
+    {
+      icon: 'mdi-apps',
+      title: 'Dashboard',
+      to: '/',
+    },
+    {
+      icon: 'mdi-chart-bubble',
+      title: 'Inspire',
+      to: '/inspire',
+    },
+    {
+      icon: 'mdi-chart-bubble',
+      title: 'Inspire',
+      to: '/inspire',
+    },
+    {
+      icon: 'mdi-chart-bubble',
+      title: 'Inspire',
+      to: '/inspire',
+    },
+    {
+      icon: 'mdi-chart-bubble',
+      title: 'Inspire',
+      to: '/inspire',
+    },
+  ],
+})
